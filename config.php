bd49bd35cca3e07517f54b98179d8485131de5e2
Rick Burgess <git@rickburgess.me>
2013-02-19 23:43:48 +0000

    boxfile

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $table_prefix = 'phpbb_';
 $acm_type = 'file';
 $load_extensions = '';
 
-@define('PHPBB_INSTALLED', true);
+@define('PHPBB_INSTALLED', false);
 // @define('DEBUG', true);
 // @define('DEBUG_EXTRA', true);
 
