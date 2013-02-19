4a7d13a930413b727df06d629a908389d33dfb12
Rick Burgess <git@rickburgess.me>
2013-02-19 23:51:30 +0000

    boxfile

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'tunnel.pagodabox.com';
+$dbport = '3306';
+$dbname = $_ENV['DB1_NAME'];
+$dbuser = $_ENV['DB1_USER'];
+$dbpasswd = $_ENV['DB1_PASS'];
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+
